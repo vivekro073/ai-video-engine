d1c404b2302c3f0462cf558978f4746cb495165a
Vivek <vivekro073@gmail.com>
2026-04-04 02:06:30 +0530

    updated youtube cookies.

--- youtube_fetcher.py
+++ youtube_fetcher.py
@@ -4,13 +4,14 @@ ydl_opts = {
     'format': 'b[ext=mp4]',
     'cookiefile': 'cookies.txt',
     'outtmpl': 'downloaded_podcast.%(ext)s',
-    'quiet': True,          # Silences standard output
-    'no_warnings': True     # Silences the JS runtime warning
+    'quiet': True,
+    'no_warnings': True,
+    'extractor_args': {
+        'youtube': ['player_client=android']
 }
 
 
 def download_video(youtube_url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([youtube_url])
-
         return "downloaded_podcast.mp4"
